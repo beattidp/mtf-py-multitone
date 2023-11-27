b170b8122adf6b02c69f41f7c8da57208196084a
Douglas Beattie <beattidp@ieee.org>
2023-11-26 18:18:43 -0700

    first try at multitone using 2 different frequencies, the amplitude still needs to be adjusted to account for summing them

--- multitone-gen.py
+++ multitone-gen.py
@@ -1,5 +1,7 @@
-import sounddevice as sd
-import numpy as np
+import sounddevice as sd    # https://python-sounddevice.readthedocs.io/en/
+import numpy as np          # https://numpy.org/doc/stable/reference/
+import soundfile as sf      # https://python-soundfile.readthedocs.io/
+from typing import Type
 
 
 
@@ -9,14 +11,29 @@ class Tone:
         self.hertz = hertz
         self.amplitude = amplitude_pct / 100
 
-d = Tone(880,0.5)
+d = Tone(880.,0.5)
+d2 = Tone(2*523.25,0.5)
 
 input_device, output_device = sd.default.device
 output_defaults = sd.query_devices(device=output_device)
 sr = sd.default.samplerate = output_defaults["default_samplerate"]
 
-sample_set = np.arange(d.seconds * sr)
-tone = np.sin(2 * np.pi * sample_set * d.hertz / sr)
-tone = tone * d.amplitude
+def generate_tone(d: Type[Tone]):
+    sample_set = np.arange(d.seconds * sr)
+    tone = np.sin(2 * np.pi * sample_set * d.hertz / sr)
+    tone = tone * d.amplitude
+    return tone
 
-sd.play(tone,sr)
+
+tone1 = generate_tone(d)
+tone2 = generate_tone(d2)
+
+sr = int(sr)
+sf.write('tone1.ogg', tone1, sr, 'VORBIS')
+sd.play(tone1)
+
+sf.write('tone2.ogg', tone2, sr, 'VORBIS')
+sd.play(tone2)
+
+sf.write('tone3.ogg', tone1+tone2, sr, 'VORBIS')
+sd.play(tone1+tone2)
